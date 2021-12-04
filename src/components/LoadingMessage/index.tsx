b46598f472ca880f7114c956ed0b2e697a99a08c
Leonardo <leonardo.zuin@oceaning.com.br>
2021-12-04 19:49:11 -0300

    dev: setting timeout to loading message

--- src/components/LoadingMessage/index.tsx
+++ src/components/LoadingMessage/index.tsx
@@ -1,16 +1,40 @@
+import { useContext, useEffect, useState } from 'react'
+import { MoodContext } from '../../contexts/MoodContext'
 import * as S from './styles'
 
-interface LoadingMessageProps {
-    message?: string
-}
+const LoadingMessage = () => {
+    const { currentMood } = useContext(MoodContext)
+    const [show, setShow] = useState(false)
+    const [showTimer, setShowTimer] = useState<any>(null)
 
-const LoadingMessage = (props: LoadingMessageProps) => {
+    useEffect(() => {
+        if(currentMood) {
+            handleCurrentMoodChange()
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentMood])
+
+    const handleCurrentMoodChange = () => {
+        setShow(true)
+
+        setShowTimer(setTimeout(resetShow, 5000))
+    }
+
+    const resetShow = () => {
+        setShow(false)
+
+        clearTimeout(showTimer)
+    }
+
     return (
-        <S.Wrapper>
-            <S.LoadingMessage>
-                {props.message || "Buscando"}...
-            </S.LoadingMessage>
-        </S.Wrapper>
+        show ?
+            <S.Wrapper>
+                <S.LoadingMessage>
+                    I'm looking for the perfect poem for feeling {currentMood}
+                </S.LoadingMessage>
+            </S.Wrapper>
+        :
+            null
     )
 }
 
